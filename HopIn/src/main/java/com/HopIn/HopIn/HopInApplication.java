f9ec8483bb65c5cb25c2ad5ee5aaf277ca37e876
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-01 00:20:05 +0100

    created project

---
+++ HopIn/src/main/java/com/HopIn/HopIn/HopInApplication.java
@@ -0,0 +1,13 @@
+package com.HopIn.HopIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopInApplication.class, args);
+	}
+
+}
---
+++ HopIn/src/test/java/com/HopIn/HopIn/HopInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HopIn.HopIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
