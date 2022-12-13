f6ca6e4a54477af0635a8b5ee7f43cb1fcb52576
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-13 22:29:23 +0100

    added panic repository and service with corresponding interface

---
+++ HopIn/src/main/java/com/hopin/HopIn/services/PanicServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hopin.HopIn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hopin.HopIn.repositories.PanicRepository;
+import com.hopin.HopIn.services.interfaces.IPanicService;
+
+public class PanicServiceImpl implements IPanicService {
+	
+	@Autowired
+	private PanicRepository panicRepository;
+
+}
