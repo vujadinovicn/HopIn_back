58af9e773e2c51259da23ed2561f683b663b969b
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-13 21:52:22 +0100

    added driver repo and injected it to service

---
+++ HopIn/src/main/java/com/hopin/HopIn/repositories/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.hopin.HopIn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hopin.HopIn.entities.Driver;
+
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+}
