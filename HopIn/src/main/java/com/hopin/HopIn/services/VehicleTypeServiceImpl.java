7fd33e86351c89d58da5f176d27c63f0ac9109db
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-13 22:31:11 +0100

    added vehicle type repository and service with corresponding interface

---
+++ HopIn/src/main/java/com/hopin/HopIn/services/VehicleTypeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hopin.HopIn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hopin.HopIn.repositories.VehicleTypeRepository;
+import com.hopin.HopIn.services.interfaces.IVehicleTypeService;
+
+public class VehicleTypeServiceImpl implements IVehicleTypeService {
+	
+	@Autowired
+	private VehicleTypeRepository vehicleTypeRepository;
+}
