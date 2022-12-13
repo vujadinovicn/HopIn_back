ad97466b7437ea453b8b8665b5e8c0ffa87289e8
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-13 21:53:54 +0100

    added vehicle repo and injected it to service

--- HopIn/src/main/java/com/hopin/HopIn/services/VehicleServiceImpl.java
+++ HopIn/src/main/java/com/hopin/HopIn/services/VehicleServiceImpl.java
@@ -3,21 +3,26 @@ package com.hopin.HopIn.services;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hopin.HopIn.dtos.LocationNoIdDTO;
 import com.hopin.HopIn.entities.Vehicle;
+import com.hopin.HopIn.repositories.VehicleRepository;
 import com.hopin.HopIn.services.interfaces.IVehicleService;
 
 @Service
 public class VehicleServiceImpl implements IVehicleService {
 	
-	private Map<Integer, Vehicle> allVehicles = new HashMap<Integer, Vehicle>();
+	@Autowired
+	VehicleRepository allVehicles;
+	
+	private Map<Integer, Vehicle> allVehiclesMap = new HashMap<Integer, Vehicle>();
 	int currId = 0;
 
 	@Override
 	public boolean updateLocation(int vehicleId, LocationNoIdDTO newLocation) {
-		Vehicle vehicle = allVehicles.get(vehicleId);
+		Vehicle vehicle = allVehiclesMap.get(vehicleId);
 //		if (vehicle != null)
 //			vehicle.setCurrentLocation(newLocation);
 		return true;
