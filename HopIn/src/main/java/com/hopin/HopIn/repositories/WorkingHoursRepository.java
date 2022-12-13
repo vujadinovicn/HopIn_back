466710c035519a0a5219eac508b3108f22b55700
tince250 <t.s.mihajlovic25@gmail.com>
2022-12-13 21:55:18 +0100

    added working hours repo, no corresponding service atm

---
+++ HopIn/src/main/java/com/hopin/HopIn/repositories/WorkingHoursRepository.java
@@ -0,0 +1,9 @@
+package com.hopin.HopIn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hopin.HopIn.entities.WorkingHours;
+
+public interface WorkingHoursRepository extends JpaRepository<WorkingHours, Integer> {
+
+}
