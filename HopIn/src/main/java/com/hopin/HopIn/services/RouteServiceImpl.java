eb58343dacddbb9cf352963f4bfeeb62fd19c9e7
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-13 22:30:56 +0100

    added route repository and service with corresponding interface

---
+++ HopIn/src/main/java/com/hopin/HopIn/services/RouteServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hopin.HopIn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hopin.HopIn.repositories.RouteRepository;
+import com.hopin.HopIn.services.interfaces.IRouteService;
+
+public class RouteServiceImpl implements IRouteService {
+	
+	@Autowired
+	private RouteRepository routeRepository;
+}
