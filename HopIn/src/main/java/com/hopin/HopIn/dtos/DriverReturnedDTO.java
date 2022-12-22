8fb86bd5bbca883a47d32d1b689230cb534d8828
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-12-22 12:18:44 +0100

    added new attributes to userReturnedDTO for vehicle info if requested user is driver

--- HopIn/src/main/java/com/hopin/HopIn/dtos/DriverReturnedDTO.java
+++ HopIn/src/main/java/com/hopin/HopIn/dtos/DriverReturnedDTO.java
@@ -1,9 +1,8 @@
 package com.hopin.HopIn.dtos;
 
 import com.hopin.HopIn.entities.User;
-import com.hopin.HopIn.entities.Vehicle;
 
-public class UserReturnedDTO {
+public class DriverReturnedDTO {
 	private int id;
 	private String name;
 	private String surname;
@@ -12,29 +11,21 @@ public class UserReturnedDTO {
 	private String email;
 	private String address;
 	private String password;
-	private String model;
-	private String licenseNumber;
-	private String vehicleType;
 	
-	
-
-	public UserReturnedDTO(int id, String name, String surname, String profilePicture, String telephoneNumber,
-			String email, String address, String password, String model, String licenseNumber, String vehicleType) {
+	public DriverReturnedDTO(int id, String name, String surname, String email, String address, String telephoneNumber,
+			String profilePicture, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
-		this.profilePicture = profilePicture;
-		this.telephoneNumber = telephoneNumber;
 		this.email = email;
 		this.address = address;
+		this.telephoneNumber = telephoneNumber;
+		this.profilePicture = profilePicture;
 		this.password = password;
-		this.model = model;
-		this.licenseNumber = licenseNumber;
-		this.vehicleType = vehicleType;
 	}
 
-	public UserReturnedDTO(User user) {
+	public DriverReturnedDTO(User user) {
 		super();
 		this.id = user.getId();
 		this.name = user.getName();
@@ -44,45 +35,6 @@ public class UserReturnedDTO {
 		this.telephoneNumber = user.getTelephoneNumber();
 		this.profilePicture = user.getProfilePicture();
 		this.password = user.getPassword();
-	}
-	
-	public UserReturnedDTO(User user, Vehicle vehicle) {
-		super();
-		this.id = user.getId();
-		this.name = user.getName();
-		this.surname = user.getSurname();
-		this.email = user.getEmail();
-		this.address = user.getAddress();
-		this.telephoneNumber = user.getTelephoneNumber();
-		this.profilePicture = user.getProfilePicture();
-		this.password = user.getPassword();
-		this.model = vehicle.getModel();
-		this.licenseNumber = vehicle.getLicenseNumber();
-		this.vehicleType = vehicle.getVehicleType().getName().toString();
-	}
-	
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public String getLicenseNumber() {
-		return licenseNumber;
-	}
-
-	public void setLicenseNumber(String licenseNumber) {
-		this.licenseNumber = licenseNumber;
-	}
-
-	public String getVehicleType() {
-		return vehicleType;
-	}
-
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
 	}
 
 	public int getId() {
@@ -148,4 +100,5 @@ public class UserReturnedDTO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 }
