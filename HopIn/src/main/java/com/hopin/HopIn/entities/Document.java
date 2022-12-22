7e54405b6bc4f823a07faf2856ab6e0e0bb36212
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-12-22 03:53:35 +0100

    changed type of document image from string to byte[]

--- HopIn/src/main/java/com/hopin/HopIn/entities/Document.java
+++ HopIn/src/main/java/com/hopin/HopIn/entities/Document.java
@@ -1,10 +1,15 @@
 package com.hopin.HopIn.entities;
 
+import java.io.UnsupportedEncodingException;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "documents")
@@ -13,13 +18,17 @@ public class Document {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
-	private String documentImage;
+	
+	@Lob
+	private byte[] documentImage;
+	
 	private int driverId;
 	
 	public Document() {}
 	
-	public Document(int id, String name, String documentImage, int driverId) {
+	public Document(int id, String name, byte[] documentImage, int driverId) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -43,12 +52,33 @@ public class Document {
 		this.name = name;
 	}
 
-	public String getDocumentImage() {
-		return documentImage;
+	public String getDocumentImage(){
+		String s;
+		try {
+			s = "data:image/jpeg;base64, ";
+			s = s + new String(this.documentImage, "UTF-8");
+//					Base64.getEncoder().encodeToString(this.profilePicture, "UTF-8");
+			return s;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public void setDocumentImage(String documentImage) {
-		this.documentImage = documentImage;
+		String[] picture = documentImage.split(",");
+		if (picture.length >= 2) {
+			//byte[] decoded = Base64.getDecoder().decode(picture[1], "-8");
+			byte[] decoded;
+			try {
+				decoded = picture[1].getBytes("UTF-8");
+				this.documentImage = decoded;
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public int getDriverId() {
