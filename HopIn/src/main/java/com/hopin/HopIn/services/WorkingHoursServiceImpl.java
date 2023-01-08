1f5a55a2cc3c5ad855c96915b79ac1f4f5637cd8
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-08 23:55:55 +0100

    created method for updating workingHours by setting end time

--- HopIn/src/main/java/com/hopin/HopIn/services/WorkingHoursServiceImpl.java
+++ HopIn/src/main/java/com/hopin/HopIn/services/WorkingHoursServiceImpl.java
@@ -1,6 +1,9 @@
 package com.hopin.HopIn.services;
 
+import java.util.Optional;
+
 import com.hopin.HopIn.dtos.WorkingHoursDTO;
+import com.hopin.HopIn.dtos.WorkingHoursEndDTO;
 import com.hopin.HopIn.dtos.WorkingHoursStartDTO;
 import com.hopin.HopIn.entities.WorkingHours;
 import com.hopin.HopIn.repositories.WorkingHoursRepository;
@@ -16,6 +19,18 @@ public class WorkingHoursServiceImpl implements IWorkingHoursService {
 		this.allWorkingHours.save(workingHours);
 		this.allWorkingHours.flush();
 		return new WorkingHoursDTO(workingHours);
+	}
+	
+	@Override
+	public WorkingHoursDTO updateWorkingHours(int id, WorkingHoursEndDTO dto) {
+		Optional<WorkingHours> found = this.allWorkingHours.findById(id);
+		if (found.isEmpty()) {
+			return null;
+		}
+		found.get().setEnd(dto.getEnd());
+		this.allWorkingHours.save(found.get());
+		this.allWorkingHours.flush();
+		return new WorkingHoursDTO(found.get());
 	}
 
 }
--- HopIn/src/main/java/com/hopin/HopIn/services/interfaces/IWorkingHoursService.java
+++ HopIn/src/main/java/com/hopin/HopIn/services/interfaces/IWorkingHoursService.java
@@ -1,10 +1,13 @@
 package com.hopin.HopIn.services.interfaces;
 
 import com.hopin.HopIn.dtos.WorkingHoursDTO;
+import com.hopin.HopIn.dtos.WorkingHoursEndDTO;
 import com.hopin.HopIn.dtos.WorkingHoursStartDTO;
 
 public interface IWorkingHoursService {
 
 	WorkingHoursDTO addWorkingHours(int driverId, WorkingHoursStartDTO dto);
+
+	WorkingHoursDTO updateWorkingHours(int id, WorkingHoursEndDTO dto);
 
 }
