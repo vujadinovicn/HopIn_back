e0556a4e649e65808f83400a0c88e5ec62aee70d
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-09 18:28:33 +0100

    changed query in working-hours-repo method findByDriverAndDates

--- HopIn/src/main/java/com/hopin/HopIn/repositories/WorkingHoursRepository.java
+++ HopIn/src/main/java/com/hopin/HopIn/repositories/WorkingHoursRepository.java
@@ -1,6 +1,6 @@
 package com.hopin.HopIn.repositories;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +10,6 @@ import com.hopin.HopIn.entities.WorkingHours;
 
 public interface WorkingHoursRepository extends JpaRepository<WorkingHours, Integer> {
 	
-	@Query(value = "select * from \"working_hours\" where \"driver_id\" = :id and cast(\"start\" as date) = :date", nativeQuery=true)
-	public List<WorkingHours> findByDriverAndStart(int id, LocalDate date);
+	@Query(value = "select * from \"working_hours\" where \"driver_id\" = :id and \"end\" between :start and :end ", nativeQuery=true)
+	public List<WorkingHours> findByDriverAndDates(int id, LocalDateTime start, LocalDateTime end);
 }
