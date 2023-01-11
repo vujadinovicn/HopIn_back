c960a71502e873a74a2ac386c5212ee698a167c6
tince250 <t.s.mihajlovic25@gmail.com>
2023-01-11 21:49:02 +0100

    moved exception handlers from validations package to rest auth controller advice to localize and resolve overriding problems

--- HopIn/src/main/java/com/hopin/HopIn/security/auth/RestAuthenticationEntryPoint.java
+++ HopIn/src/main/java/com/hopin/HopIn/security/auth/RestAuthenticationEntryPoint.java
@@ -2,15 +2,27 @@ package com.hopin.HopIn.security.auth;
 
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.hopin.HopIn.validations.ExceptionDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -22,6 +34,28 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     	System.out.println(authException);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	protected ResponseEntity<Object> handleConstraintValidationException(ConstraintViolationException e) {
+		// 400
+		return new ResponseEntity<>(new ExceptionDTO(e.getMessage().split(": ")[1]), HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler (value = {MethodArgumentNotValidException.class})
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+		// 400
+		List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        StringBuilder sb = new StringBuilder("Request finished with validation errors: \n");
+
+        for (ObjectError error : errorList ) {
+        	sb.append("Field ");
+            FieldError fe = (FieldError) error;
+            sb.append(fe.getField() + " ");
+            sb.append(error.getDefaultMessage()+ "!\n\n");
+        }
+
+        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
+	}
     
     @ExceptionHandler (value = {AccessDeniedException.class})
     public void commence(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
@@ -40,6 +74,7 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
         // 403
         setResponseError(response, HttpServletResponse.SC_FORBIDDEN, String.format("Access Denies: %s", exception.getMessage()));
     }
+    
     
     private void setResponseError(HttpServletResponse response, int errorCode, String errorMessage) throws IOException{
         response.setStatus(errorCode);
