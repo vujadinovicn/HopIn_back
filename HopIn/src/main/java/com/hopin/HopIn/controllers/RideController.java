9c00eac1f59367246834ad56032d6cb18b8a590c
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-01-17 19:07:05 +0100

    fixed ride canceling error and panic controllers for test passing

--- HopIn/src/main/java/com/hopin/HopIn/controllers/RideController.java
+++ HopIn/src/main/java/com/hopin/HopIn/controllers/RideController.java
@@ -226,10 +226,8 @@ public class RideController {
 	@PutMapping(value = "/{id}/cancel", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@PreAuthorize("hasRole('DRIVER')")
 	public ResponseEntity<?> rejectRide(
-			@PathVariable @Min(value = 0, message = "Field id must be greater than 0.") int id,
+			@PathVariable(required = false) @Min(value = 0, message = "Field id must be greater than 0.") int id,
 			@Valid @RequestBody(required=false) ReasonDTO dto) {
-		System.out.println("DECLINE REQ: " + id);
-		System.out.println("DECLINE RES: " + dto.getReason());
 		try {
 			RideReturnedDTO ride = service.rejectRide(id, dto);
 //			this.simpMessagingTemplate.convertAndSend("/topic/ride-offer-response/" + ride.getPassengers().get(0).getId(), "false");
