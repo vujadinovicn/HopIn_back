5945dbef29cd8f3a1646ecf7c70d20e853d5be49
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-19 20:42:05 +0100

    added method for getting all rides between dates

--- HopIn/src/main/java/com/hopin/HopIn/repositories/RideRepository.java
+++ HopIn/src/main/java/com/hopin/HopIn/repositories/RideRepository.java
@@ -12,6 +12,9 @@ import com.hopin.HopIn.entities.Ride;
 
 public interface RideRepository extends JpaRepository<Ride, Integer>, PagingAndSortingRepository<Ride, Integer> {
 	
+	@Query(value = "select * from \"rides\" where \"start_time\" between :from and :to order by \"start_time\"", nativeQuery=true)
+	public List<Ride> getAllRidesBetweenDates(LocalDateTime from, LocalDateTime to);
+	
 	@Query(value = "select * from \"rides\" where \"id\" in (select \"ride_id\" from \"rides_passengers\" where \"passengers_id\" = :id) and \"start_time\" between :from and :to order by \"start_time\"", nativeQuery=true)
 	public List<Ride> getAllPassengerRidesBetweenDates(int id, LocalDateTime from, LocalDateTime to);
 	
