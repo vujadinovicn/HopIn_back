c0d34bf174afc66b31c943c7b5397fbfd2a97486
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-01-22 01:10:15 +0100

    created method for getting all passenger's rides without pagination

--- HopIn/src/main/java/com/hopin/HopIn/controllers/ReviewController.java
+++ HopIn/src/main/java/com/hopin/HopIn/controllers/ReviewController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +36,7 @@ import jakarta.validation.constraints.Min;
 
 @Validated
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("/api/review")
 public class ReviewController {
 	
