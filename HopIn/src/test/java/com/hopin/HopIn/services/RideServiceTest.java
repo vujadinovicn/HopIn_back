e0861ff19ef69366c9fffc737b231a5abd98608a
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2023-02-06 03:25:58 +0100

    tested cancel ride, get price and get favorites rides methods from ride service

--- HopIn/src/test/java/com/hopin/HopIn/services/RideServiceTest.java
+++ HopIn/src/test/java/com/hopin/HopIn/services/RideServiceTest.java
@@ -7,7 +7,9 @@ import static org.mockito.Mockito.verify;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -24,12 +26,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.hopin.HopIn.HopInApplication;
+import com.hopin.HopIn.dtos.FavoriteRideReturnedDTO;
 import com.hopin.HopIn.dtos.RideReturnedDTO;
+import com.hopin.HopIn.dtos.UnregisteredRideSuggestionDTO;
 import com.hopin.HopIn.entities.Driver;
 import com.hopin.HopIn.entities.FavoriteRide;
 import com.hopin.HopIn.entities.Location;
 import com.hopin.HopIn.entities.Passenger;
 import com.hopin.HopIn.entities.Ride;
+import com.hopin.HopIn.entities.Route;
 import com.hopin.HopIn.entities.VehicleType;
 import com.hopin.HopIn.enums.RideStatus;
 import com.hopin.HopIn.enums.VehicleTypeName;
@@ -37,6 +42,7 @@ import com.hopin.HopIn.exceptions.FavoriteRideException;
 import com.hopin.HopIn.exceptions.RideNotFoundException;
 import com.hopin.HopIn.repositories.FavoriteRideRepository;
 import com.hopin.HopIn.repositories.RideRepository;
+import com.hopin.HopIn.repositories.VehicleTypeRepository;
 import com.hopin.HopIn.services.interfaces.IPassengerService;
 import com.hopin.HopIn.services.interfaces.IRideService;
 import com.hopin.HopIn.services.interfaces.IUserService;
@@ -61,12 +67,17 @@ public class RideServiceTest extends AbstractTestNGSpringContextTests {
 	@MockBean 
 	private IUserService userService;
 	
+	@MockBean
+	private VehicleTypeRepository allVehicleTypes;
+	
 	
 	private Ride ride;
 	private Passenger passenger;
 	private FavoriteRide favoriteRoute;
+	private VehicleType vehicleType;
 	
 	private static final int NON_EXISTANT_RIDE_ID = 123;
+	private static final double DISTANCE = 1;
 	
 	@BeforeMethod
 	public void setup() {
@@ -79,7 +90,7 @@ public class RideServiceTest extends AbstractTestNGSpringContextTests {
 		Set<Passenger> passengers = new HashSet<>();
 		passengers.add(passenger);
 		
-		VehicleType type = new VehicleType(1, VehicleTypeName.CAR, 60);
+		vehicleType = new VehicleType(1, VehicleTypeName.CAR, 60);
 		
 		ride = new Ride();
 		ride.setId(1);
@@ -88,7 +99,7 @@ public class RideServiceTest extends AbstractTestNGSpringContextTests {
 		ride.setDestinationLocation(destination);
 		ride.setDriver(driver);
 		ride.setPassengers(passengers);
-		ride.setVehicleType(type);
+		ride.setVehicleType(vehicleType);
 	}
 	
 	@Test
@@ -173,6 +184,7 @@ public class RideServiceTest extends AbstractTestNGSpringContextTests {
 		} catch (ResponseStatusException ex) {
 			assertEquals(ex.getStatusCode(), HttpStatus.NOT_FOUND);
 			assertEquals(ex.getReason(), "Ride does not exist!");
+			throw ex;
 		}
 	}
 	
@@ -186,8 +198,80 @@ public class RideServiceTest extends AbstractTestNGSpringContextTests {
 		} catch (ResponseStatusException ex) {
 			assertEquals(ex.getStatusCode(), HttpStatus.BAD_REQUEST);
 			assertEquals(ex.getReason(), "Cannot accept a ride that is not in status PENDING!");
+			throw ex;
 		}
 	}
+	
+	@Test
+	public void shouldCancelRide() {
+		ride.setStatus(RideStatus.PENDING);
+		Mockito.when(allRides.findById(ride.getId())).thenReturn(Optional.of(ride));
+		Mockito.when(allRides.save(ride)).thenReturn(ride);
+
+		RideReturnedDTO ret = this.rideService.cancelRide(ride.getId());
+		
+		assertTrue(ret.getId() == ride.getId());
+		assertTrue(ret.getStatus() == RideStatus.CANCELED);	
+	}
+	
+	@Test(expectedExceptions = {ResponseStatusException.class})
+	public void shouldThrowExceptionForCancelingRideWithNonExistingId() {
+		try {
+			Mockito.when(allRides.findById(anyInt())).thenReturn(Optional.empty());
+			this.rideService.cancelRide(0);
+		} catch (ResponseStatusException ex) {
+			assertEquals(ex.getStatusCode(), HttpStatus.NOT_FOUND);
+			assertEquals(ex.getReason(), "Ride does not exist!");
+			throw ex;
+		}
+	}
+	
+	@Test(expectedExceptions = {ResponseStatusException.class})
+	public void shouldThrowExceptionForCancelingRideWithBadStatus() {
+		this.ride.setStatus(RideStatus.FINISHED);		
+		Mockito.when(allRides.findById(ride.getId())).thenReturn(Optional.of(ride));
+
+		try {
+			this.rideService.cancelRide(ride.getId());
+		} catch (ResponseStatusException ex) {
+			assertEquals(ex.getStatusCode(), HttpStatus.BAD_REQUEST);
+			assertEquals(ex.getReason(), "Cannot cancel a ride that is not in status PENDING or STARTED!");
+			throw ex;
+		}
+	}
+	
+	@Test
+	public void shouldGetRideSugestionPrice() {
+		UnregisteredRideSuggestionDTO dto = new UnregisteredRideSuggestionDTO(vehicleType.getName().toString(), DISTANCE);
+		Mockito.when(allVehicleTypes.getByName(vehicleType.getName())).thenReturn(vehicleType);
+		Double ret = this.rideService.getRideSugestionPrice(dto);
+		assertEquals(ret, vehicleType.getPricePerKm());
+	}
+	
+	@Test(expectedExceptions = {NullPointerException.class})
+	public void shouldThrowExceptionWhenTryingToGetPriceButVehicleIsNull() {
+		UnregisteredRideSuggestionDTO dto = new UnregisteredRideSuggestionDTO(null, 1.0);
+		Double ret = this.rideService.getRideSugestionPrice(dto);
+	}
+	
+	
+	@Test
+	public void shouldGetFavoriteRides() {
+		Set<Route> routes = new HashSet<Route>();
+		routes.add(new Route(1, ride.getDepartureLocation(), ride.getDestinationLocation(), 10.0));
+		FavoriteRide favRide = new FavoriteRide(1, "", routes, ride.getPassengers(), vehicleType, true, true);
+		List<FavoriteRide> favRides = new ArrayList<FavoriteRide>();
+		favRides.add(favRide);
+		
+		Mockito.when(passengerService.getFavoriteRides(passenger.getId())).thenReturn(favRides);
+		Mockito.when(userService.getCurrentUser()).thenReturn(passenger);
+		
+		List<FavoriteRideReturnedDTO> ret = this.rideService.getFavoriteRides();
+		assertEquals(favRides.size(), ret.size());
+		assertEquals(ret.get(0).getId(), favRide.getId());
+	}
+	
+	
 	
 	
 	
