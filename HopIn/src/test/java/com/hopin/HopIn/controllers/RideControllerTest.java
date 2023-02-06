15599a74f719023e64232b7c11a513b792adb307
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2023-02-06 19:28:39 +0100

    added controller test methods for rejecting ride

--- HopIn/src/test/java/com/hopin/HopIn/controllers/RideControllerTest.java
+++ HopIn/src/test/java/com/hopin/HopIn/controllers/RideControllerTest.java
@@ -1,8 +1,8 @@
 package com.hopin.HopIn.controllers;
 
-
-import static org.junit.Assert.assertTrue;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertTrue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -18,10 +19,15 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hopin.HopIn.dtos.CredentialsDTO;
+import com.hopin.HopIn.dtos.ReasonDTO;
 import com.hopin.HopIn.dtos.RideReturnedDTO;
 import com.hopin.HopIn.dtos.TokenDTO;
 import com.hopin.HopIn.enums.RideStatus;
+import com.hopin.HopIn.validations.ExceptionDTO;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -43,7 +49,10 @@ public class RideControllerTest extends AbstractTestNGSpringContextTests {
 	private static String TOKEN_ADMIN;
 	
 	private static int ACCEPTED_RIDE_ID = 1;
-
+	private static int PENDING_RIDE_ID = 2;
+	private static int INVALID_RIDE_ID = -1;
+	private static int STARTED_RIDE_ID = 3;
+	private static int NON_EXISTING_RIDE_ID = 169;
 	@Autowired
     private TestRestTemplate restTemplate;
 	
@@ -62,7 +71,17 @@ public class RideControllerTest extends AbstractTestNGSpringContextTests {
 	private HttpEntity<String> makeJwtHeader(String token) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setBearerAuth(token);
+		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> entity = new HttpEntity<>(headers);
+		
+		return entity;
+	}
+	
+	private HttpEntity<String> makeJwtHeaderWithRequestBody(String body, String token) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(token);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> entity = new HttpEntity<>(body, headers);
 		
 		return entity;
 	}
@@ -91,4 +110,75 @@ public class RideControllerTest extends AbstractTestNGSpringContextTests {
 		assertEquals(ride.getId(), ACCEPTED_RIDE_ID);
 		assertTrue(ride.getStartTime() != null);
 	}
+	
+	private String convertReasonDTOToJson(String r) {
+		ReasonDTO reason = new ReasonDTO(r);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.findAndRegisterModules(); 
+		String jsonString = null;
+		
+		try {
+			jsonString = mapper.writeValueAsString(reason);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return jsonString;
+	}
+	@Test
+	public void shouldThrowUnauthorizedExceptionWhenRejectingRide() {
+		ResponseEntity<RideReturnedDTO> res = restTemplate.withBasicAuth("driver@gmail.com", "123")
+				  .exchange("/api/ride/" + PENDING_RIDE_ID + "/cancel", HttpMethod.PUT, null, RideReturnedDTO.class);
+		assertEquals(res.getStatusCode(), HttpStatus.UNAUTHORIZED);
+	}
+	
+	@Test
+	public void shouldThrowForbiddenExceptionWhenRejectingRideAsAdmin() {
+		ResponseEntity<String> res = restTemplate.exchange("/api/ride/" + PENDING_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeader(TOKEN_ADMIN), String.class);
+		assertEquals(res.getStatusCode(), HttpStatus.FORBIDDEN);
+	}
+	
+	@Test
+	public void shouldThrowJSONExceptionWhenRejectingRideWithInvalidRideId() {
+		ResponseEntity<String> res = restTemplate.exchange("/api/ride/" + INVALID_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeader(TOKEN_DRIVER), String.class);
+		assertNotEquals(res.getStatusCode(), HttpStatus.OK);
+	}
+	
+	@Test
+	public void shouldThrowJSONExceptionWhenRejectingRideWithInvalidReason() {
+		ResponseEntity<String> res = restTemplate.withBasicAuth("driver@gmail.com", "123").exchange
+				("/api/ride/" + PENDING_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeaderWithRequestBody(convertReasonDTOToJson("reason"), TOKEN_DRIVER), String.class);
+		assertNotEquals(res.getStatusCode(), HttpStatus.OK);
+	}
+	
+	@Test
+	public void shouldThrowBadRequestExceptionWhenRejectingRideWithInvalidStatus() {
+		ResponseEntity<ExceptionDTO> res = restTemplate.withBasicAuth("driver@gmail.com", "123")
+				  .exchange("/api/ride/" + STARTED_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeaderWithRequestBody(convertReasonDTOToJson("reason"), TOKEN_DRIVER), ExceptionDTO.class);
+		assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+		assertEquals(res.getBody().getMessage(), "Cannot cancel a ride that is not in status PENDING or ACCEPTED!");
+	}
+	
+	@Test
+	public void shouldThrowNotFoundExceptionWhenRejectingRideForNonExistingRide() {
+		ResponseEntity<String> res = restTemplate.withBasicAuth("driver@gmail.com", "123")
+				  .exchange("/api/ride/" + NON_EXISTING_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeaderWithRequestBody(convertReasonDTOToJson("reason"), TOKEN_DRIVER), String.class);
+		assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
+		assertEquals(res.getBody(), "Ride does not exist!");
+	}
+	
+	
+	@Test
+	public void shouldRejectRide() {
+		ResponseEntity<RideReturnedDTO> res = restTemplate.withBasicAuth("driver@gmail.com", "123")
+				  .exchange("/api/ride/" + PENDING_RIDE_ID + "/cancel", HttpMethod.PUT, makeJwtHeaderWithRequestBody(convertReasonDTOToJson("reason"), TOKEN_DRIVER), RideReturnedDTO.class);
+		
+		RideReturnedDTO ride = res.getBody();
+		
+		assertEquals(HttpStatus.OK, res.getStatusCode());
+		assertEquals(ride.getStatus(), RideStatus.REJECTED);
+		assertEquals(ride.getId(), PENDING_RIDE_ID);
+		assertTrue(ride.getStartTime() == null);
+	}
+	
+	
 }
